55bd815fb28ebfd79fb58766b7683e1de4d66e89
Pierre-Henri <phbasic@gmail.com>
2024-06-04 11:21:23 +0200

    feat: ordered packages

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -3,111 +3,119 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [
-    cmake
-    gcc
-    gnumake
-    gnupg
+home.packages = with pkgs; [
+  # Build Tools
+  cmake        # Cross-platform build system
+  gcc          # GNU Compiler Collection
+  gnupg        # GNU Privacy Guard for encrypting and signing data
+  gnumake      # Control the generation of executables
 
-    wget
-    curl
+  # Networking Tools
+  curl         # Command-line tool for transferring data with URLs
+  ethtool      # Display and modify network interface controller parameters
+  gping        # Ping with a graph
+  iftop        # Real-time console-based network bandwidth monitoring
+  pciutils     # List and manipulate PCI devices
+  usbutils     # Manage USB devices
+  wget         # Network utility to retrieve files from the web
 
-    # Git
-    git
-    lazygit
+  # Version Control
+  git          # Distributed version control system
+  gitflow      # Extensions to Git for managing workflows
+  lazygit      # Simple terminal UI for Git commands
 
-    # System
-    sysstat
-    lm_sensors # for `sensors` command
-    neofetch
-    xfce.thunar # xfce4's file manager
-    nnn # terminal file manager
-    ethtool
-    pciutils # lspci
-    usbutils # lsusb
-    btop  # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
-    gping # ping but with graph
-    duf # disk usage
+  # System Monitoring and Management
+  btop         # Resource monitor replacing htop/nmon
+  duf          # Disk usage utility with fancy output
+  iftop        # Real-time console-based network bandwidth monitoring
+  iotop        # Top-like UI for monitoring disk I/O
+  lm_sensors   # Read temperature/voltage/fan sensors
+  lsof         # List open files
+  ltrace       # Display calls to dynamic libraries
+  neofetch     # Command-line system information tool
+  nnn          # Terminal file manager
+  pciutils     # List and manipulate PCI devices
+  strace       # Monitor system calls
+  sysstat      # Performance monitoring tools for Linux
+  usbutils     # Manage USB devices
+  xfce.thunar  # XFCE's file manager
 
+  # Rust Development
+  pkg-config   # Needed by some Rust libraries
 
-    # Needed by some Rust lib
-    pkg-config
+  # Browsers
+  firefox      # Web browser
+  lynx         # Text-based web browser
 
-    # Browser 
-    firefox
-    lynx
+  # Archive Tools
+  p7zip        # File archiver with a high compression ratio
+  unzip        # Extract zip archives
+  zip          # Create zip archives
 
-    # Archives
-    zip
-    unzip
-    p7zip
+  # Utilities
+  eza          # Modern replacement for 'ls'
+  fzf          # Command-line fuzzy finder
+  ripgrep      # Line-oriented search tool
+  xclip        # Command line interface to the X11 clipboard
 
-    # Utils
-    ripgrep
-    eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
-    xclip
+  # IDEs
+  insomnia     # API Client for GraphQL, REST, and gRPC
+  vscode       # Visual Studio Code
 
-    # IDE
-    insomnia
-    vscode
+  # Image Editing
+  gimp         # GNU Image Manipulation Program
 
-    # Cloud native
-    docker
-    kubectl
-    terraform
+  # Audio Editing
+  audacity     # Audio editor and recorder
 
-    # Misc
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    libnotify
-    xdg-utils
+  # Cloud Native Tools
+  docker       # Platform for developing, shipping, and running applications
+  kubectl      # Command-line tool for controlling Kubernetes clusters
+  terraform    # Infrastructure as code tool
 
-    # productivity
-    glow # markdown previewer in terminal
+  # Miscellaneous
+  file         # Determine file type
+  figlet       # Generate ASCII art from text
+  gawk         # Pattern scanning and processing language
+  glow         # Terminal-based markdown viewer
+  gnused       # GNU version of the stream editor
+  gnutar       # GNU version of tar archiving utility
+  libnotify    # Library for sending desktop notifications
+  tree         # Display directories as trees
+  xdg-utils    # Utilities for managing X desktop environments
+  zstd         # Fast lossless compression algorithm
 
-    # shell
-    nushell
-    carapace
+  # Shells
+  carapace     # Completion generator for Bash, Zsh, Fish, and PowerShell
+  nushell      # Modern shell for the GitHub era
 
-    # Communication
-    slack
+  # Communication
+  slack        # Collaboration software
 
-    # rust
-    # rustup
-    rust-analyzer
-    cargo
-    rustfmt
+  # Sound Management
+  easyeffects  # Audio effects for PipeWire applications
 
-    # Python
-    poetry
-    nodePackages.pyright # python language server
-    (python312.withPackages (
-      ps:
-        with ps; [
-          ruff-lsp
-          requests
-        ]
-    ))
+  # Rust Development Tools
+  cargo         # Rust package manager
+  rust-analyzer # Rust language server
+  rustfmt       # Rust code formatter
 
-    nodejs
+  # Python Development Tools
+  nodePackages.pyright # Python language server
+  poetry          # Python dependency management and packaging
+  (python312.withPackages (ps: with ps; [
+    requests      # HTTP library for Python
+    ruff-lsp      # Language server for the Ruff Python linter
+  ]))
 
-    # Lua
-    stylua
-    lua-language-server
+  # JavaScript and Node.js
+  nodejs         # JavaScript runtime
 
-    tree-sitter 
+  # Lua Development Tools
+  lua-language-server # Language server for Lua
+  stylua         # Lua code formatter
 
+  tree-sitter    # Parser generator tool and an incremental parsing library
 ];
 
   programs = {
