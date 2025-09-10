75ba2ccab06d21777b19593fd30546eb9bcb3052
Pierre-Henri <phbasic@gmail.com>
2025-09-10 16:42:45 +0200

    feat: refactoring

--- modules/system.nix
+++ modules/system.nix
@@ -7,102 +7,29 @@ let
   username = "ph";
 in
 {
-  # --- Users ---
-  users.users.ph = {
-    isNormalUser = true;
-    description = "ph";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-  };
-
-  # Nix settings
-  nix.settings.trusted-users = [ username ];
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  environment = {
+    systemPackages = with pkgs; [
+      gnome-settings-daemon
+      vim
+      config.boot.kernelPackages.perf
+      # Hyprland
+      wofi
+      pamixer
+      brightnessctl
+      wl-clipboard
+      udiskie
+      networkmanagerapplet
+      blueman
+      hyprlock
+      hypridle
     ];
-    builders-use-substitutes = true;
+
+    variables = {
+      EDITOR = "nvim";
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    };
   };
 
-  # GC old generations
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    dates = lib.mkDefault "weekly";
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
-
-  # Allow unfree (Steam, etc.)
-  nixpkgs.config.allowUnfree = true;
-
-  # Locale / TZ
-  time.timeZone = "Europe/Paris";
-  i18n.defaultLocale = "fr_FR.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-  };
-
-  # --- Display manager / desktops ---
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true; # Choose "Hyprland" at login
-  services.xserver.desktopManager.gnome.enable = false; # Keep GNOME available
-  services.hypridle.enable = true;
-
-  # Hyprland session (system-provided wrapper)
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  # --- Graphics & audio stack ---
-  hardware.graphics = { enable = true; enable32Bit = true; };
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-  services.pulseaudio.enable = false;
-
-  # --- Portals (Wayland screenshare / file pickers) ---
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  # With GNOME installed, prefer the Hyprland portal to avoid conflicts:
-  xdg.portal.config.common.default = "hyprland";
-
-  # --- Polkit, keyring, storage (udiskie) ---
-  security.polkit.enable = true;
-  services.gnome.gnome-keyring.enable = true;
-  services.udisks2.enable = true;
-
-  # --- Hyprlock + fingerprint ---
-  services.fprintd.enable = true;
-  security.pam.services.hyprlock = { };
-
-  services.gvfs.enable = true;
-  services.tumbler.enable = true;
-
-  # --- Power / sleep policies ---
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
-
-  # --- Printing ---
-  services.printing.enable = true;
-
-  # --- Fonts ---
   fonts = {
     packages = with pkgs; [
       material-design-icons
@@ -121,55 +48,129 @@ in
     };
   };
 
-  # --- Misc desktop plumbing ---
-  programs.dconf.enable = true;
-  services.dbus.packages = [ pkgs.gcr ];
-  services.geoclue2.enable = true;
-
-  # Hyprland mess
-  services.dbus.enable = true;
-
-  # --- Networking / firewall ---
-  networking.firewall.enable = false;
-
-  # --- Packages expected by your HM Hyprland config ---
-  environment.systemPackages = with pkgs; [
-    # your originals
-    gnome-settings-daemon
-    vim
-    config.boot.kernelPackages.perf
-
-    # Hyprland
-    wofi
-    pamixer
-    brightnessctl
-    wl-clipboard
-    udiskie
-    networkmanagerapplet
-    blueman
-    hyprlock
-    hypridle
-  ];
-
-  programs.thunar.enable = true;
-  # --- Env vars ---
-  environment.variables = {
-    EDITOR = "nvim";
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
   };
 
-  # --- Containers / gaming / extras ---
-  virtualisation.docker.enable = true;
-  programs.steam.enable = true;
-  services.ollama.enable = true;
+  i18n = {
+    defaultLocale = "fr_FR.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "fr_FR.UTF-8";
+      LC_IDENTIFICATION = "fr_FR.UTF-8";
+      LC_MEASUREMENT = "fr_FR.UTF-8";
+      LC_MONETARY = "fr_FR.UTF-8";
+      LC_NAME = "fr_FR.UTF-8";
+      LC_NUMERIC = "fr_FR.UTF-8";
+      LC_PAPER = "fr_FR.UTF-8";
+      LC_TELEPHONE = "fr_FR.UTF-8";
+      LC_TIME = "fr_FR.UTF-8";
+    };
+  };
 
-  # --- Power profile ---
-  services.power-profiles-daemon.enable = true;
+  networking.firewall.enable = false;
+
+  nix = {
+    gc = {
+      automatic = lib.mkDefault true;
+      dates = lib.mkDefault "weekly";
+      options = lib.mkDefault "--delete-older-than 7d";
+    };
+
+    settings = {
+      builders-use-substitutes = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+      trusted-users = [ username ];
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
   powerManagement.cpuFreqGovernor = "performance";
-  systemd.services."set-performance-profile" = {
-    description = "Set power profile to performance";
-    after = [ "power-profiles-daemon.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig.ExecStart = "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance";
+
+  programs = {
+    dconf.enable = true;
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+    steam.enable = true;
+    thunar.enable = true;
+  };
+
+  security = {
+    pam.services.hyprlock = { };
+    polkit.enable = true;
+    rtkit.enable = true;
+  };
+
+  services = {
+    dbus = {
+      enable = true;
+      packages = [ pkgs.gcr ];
+    };
+
+    fprintd.enable = true;
+    geoclue2.enable = true;
+    gnome.gnome-keyring.enable = true;
+    gvfs.enable = true;
+    ollama.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    power-profiles-daemon.enable = true;
+    printing.enable = true;
+
+    pulseaudio.enable = false;
+
+    tumbler.enable = true;
+    udisks2.enable = true;
+
+    xserver = {
+      enable = true;
+      desktopManager.gnome.enable = false; # Keep GNOME available
+      displayManager.gdm.enable = true; # Choose "Hyprland" at login
+    };
+  };
+
+  systemd = {
+    services."set-performance-profile" = {
+      description = "Set power profile to performance";
+      after = [ "power-profiles-daemon.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.ExecStart = "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance";
+    };
+
+    sleep.extraConfig = ''
+      AllowSuspend=no
+      AllowHibernation=no
+      AllowHybridSleep=no
+      AllowSuspendThenHibernate=no
+    '';
+  };
+
+  time.timeZone = "Europe/Paris";
+
+  users.users.ph = {
+    isNormalUser = true;
+    description = "ph";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+  };
+
+  virtualisation.docker.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+    config.common.default = "hyprland"; # Prefer Hyprland portal over GNOME
   };
 }
