ec762b6a4cd5153941a6cd39e3a17a4d1de683f7
Pierre-Henri <phbasic@gmail.com>
2025-09-10 14:02:08 +0200

    feat: bar working on startup

--- home/hyprland/systemd.nix
+++ home/hyprland/systemd.nix
@@ -2,37 +2,39 @@
 {
   home.packages = with pkgs; [ eww ];
 
-  systemd.user.services.eww = {
-    Unit = {
-      Description = "Eww daemon";
-      After = [ "graphical.target" ];
-    };
-    Service = {
-      ExecStart = "${pkgs.eww}/bin/eww daemon";
-      Restart = "always";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
   home.sessionVariables = {
     EWW_CONFIG_DIR = "${config.home.homeDirectory}/.config/eww";
   };
 
-  # Start bar after login
+  systemd.user.services."eww" = {
+    Unit = {
+      Description = "Eww daemon";
+      After = [ "graphical-session.target" ];
+      Wants = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.eww}/bin/eww daemon --no-daemonize";
+      Restart = "always";
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
   systemd.user.services."eww-bar" = {
     Unit = {
       Description = "Open Eww bar";
-      After = [ "eww.service" ];
-      Wants = [ "eww.service" ];
+      After = [ "eww.service" "graphical-session.target" ];
+      Wants = [ "eww.service" "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
     };
     Service = {
       ExecStart = "${pkgs.eww}/bin/eww open bar";
       Restart = "on-failure";
     };
     Install = {
-      WantedBy = [ "default.target" ];
+      WantedBy = [ "graphical-session.target" ];
     };
   };
 }
