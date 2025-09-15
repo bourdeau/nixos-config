480d306d944d7569c88913ef276c194a0b925c69
Pierre-Henri <phbasic@gmail.com>
2025-09-15 15:28:07 +0200

    feat: Bitwig Studio with VST plugins loading (#18)

--- hosts/phantec/home/bitwig-studio/default.nix
+++ hosts/phantec/home/bitwig-studio/default.nix
@@ -1,27 +1,52 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    bitwig-studio
+{pkgs, ...}: let
+  # Wrap Bitwig Studio inside a FHS-compatible environment (bubblewrap sandbox).
+  # This is needed because Bitwig loads VST plugins, which are precompiled binaries
+  # expecting the "standard Linux filesystem layout" and dynamic libraries to be in
+  # /usr/lib-style locations. NixOS doesn’t provide that by default.
+  bitwig-with-plugins = pkgs.buildFHSEnvBubblewrap {
+    name = "bitwig-with-plugins";
+
+    # targetPkgs = the packages available inside the FHS environment.
+    # Here we include:
+    # - Bitwig itself
+    # - lsp-plugins (test case for plugins)
+    # - all libraries required by the plugins (libsndfile, cairo, X11, GL, etc.)
+    targetPkgs = pkgs: [
+      pkgs.bitwig-studio
+      pkgs.lsp-plugins
+
+      pkgs.libsndfile # needed by many audio plugins
+      pkgs.cairo # graphics/text rendering dependency
+      pkgs.fontconfig.lib
+      pkgs.freetype
+      pkgs.libGL
+      pkgs.xorg.libX11
+      pkgs.xorg.libXrandr
+      pkgs.stdenv.cc.cc.lib # provides libstdc++.so for C++ plugins
+    ];
+
+    runScript = "bitwig-studio";
+  };
+in {
+  # Make our wrapper available in PATH so we can run "bitwig-with-plugins"
+  home.packages = [
+    bitwig-with-plugins
   ];
 
-  hmCopyConfig = {
-    bitwig-prefs = {
-      source = ./config/prefs;
-      target = ".BitwigStudio/prefs";
-    };
-
-    bitwig-view-settings = {
-      source = ./config/view-settings;
-      target = ".BitwigStudio/view-settings";
-    };
-
-    bitwig-color-palettes = {
-      source = ./config/Color_Palettes;
-      target = "Bitwig_Studio/Color_Palettes";
-    };
-
-    bitwig-controller-scripts = {
-      source = ./config/Controller_Scripts;
-      target = "Bitwig_Studio/Controller_Scripts";
-    };
-  };
+  # Override Bitwig’s default .desktop file, because the stock one launches
+  # plain `bitwig-studio`, which doesn’t have working VSTs on NixOS.
+  # Instead, we point Exec= to our wrapped version.
+  home.file.".local/share/applications/com.bitwig.BitwigStudio.desktop".text = ''
+    [Desktop Entry]
+    Name=Bitwig Studio
+    GenericName=Digital Audio Workstation
+    Comment=Bitwig Studio with working VST plugins
+    Exec=${bitwig-with-plugins}/bin/bitwig-with-plugins %F
+    Icon=com.bitwig.BitwigStudio
+    Terminal=false
+    Type=Application
+    Categories=AudioVideo;Audio;Midi;Sequencer;
+    StartupWMClass=com.bitwig.BitwigStudio
+    MimeType=application/bitwig-project;application/bitwig-preset;
+  '';
 }
