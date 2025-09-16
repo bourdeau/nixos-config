370f2926609894f6a353b916e009e9f375839379
Pierre-Henri <phbasic@gmail.com>
2025-09-16 07:01:09 +0200

    feat: dowloading LSP plugins

--- hosts/phantec/home/bitwig-studio/default.nix
+++ hosts/phantec/home/bitwig-studio/default.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: let
+{ pkgs, config, ... }:
+let
   # We create a wrapper around Bitwig Studio using buildFHSEnvBubblewrap.
   # This provides a FHS-like environment, so that plugins and Bitwig’s
   # plugin host can find shared libraries (like libsndfile, freetype, X11, etc.)
@@ -51,9 +52,29 @@
     # (sometimes Bitwig or plugins try to spawn it).
     extraBindMounts = [
       "${pkgs.bubblewrap}/bin/bwrap:/usr/bin/bwrap"
+      "${config.home.homeDirectory}/.vst:${config.home.homeDirectory}/.vst"
+      "${config.home.homeDirectory}/vst3:${config.home.homeDirectory}/vst3"
     ];
   };
-in {
+
+  # Install LSP VSTs
+  lspPlugins = pkgs.stdenv.mkDerivation {
+    pname = "lsp-plugins";
+    version = "1.2.23";
+    src = pkgs.fetchurl {
+      url = "https://github.com/sadko4u/lsp-plugins/releases/download/1.2.23/lsp-plugins-1.2.23-Linux-x86_64.7z";
+      sha256 = "0978qab3rag63ac3irwvicbrk98y8160b2xgj1q8z04y89yzv6dx";
+    };
+    nativeBuildInputs = [ pkgs.p7zip ];
+    installPhase = ''
+      mkdir -p $out/VST2 $out/VST3
+      7z x $src
+      cp -r lsp-plugins-1.2.23-Linux-x86_64/VST2/* $out/VST2/
+      cp -r lsp-plugins-1.2.23-Linux-x86_64/VST3/* $out/VST3/
+    '';
+  };
+in
+{
   home.packages = [
     bitwig-with-plugins # Add our wrapper to PATH
   ];
@@ -72,4 +93,15 @@ in {
     StartupWMClass=com.bitwig.BitwigStudio
     MimeType=application/bitwig-project;application/bitwig-preset;
   '';
+
+  hmCopyConfig = {
+    lsp-vst2 = {
+      source = "${lspPlugins}/VST2";
+      target = ".vst";
+    };
+    lsp-vst3 = {
+      source = "${lspPlugins}/VST3";
+      target = ".vst3";
+    };
+  };
 }
