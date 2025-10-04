0d868919162f61cfb28953d7ceda885289da16b2
Pierre-Henri <phbasic@gmail.com>
2025-10-04 19:30:33 +0200

    fix: typo

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -3,11 +3,12 @@
   pkgs,
   ...
 }: {
-  sops.gnupg.home = "/root/.gnupg";
-  sops.defaultSopsFile = ./secrets.yaml;
-
-  sops.secrets.protonvpn = {
-    sopsFile = ./secrets.yaml;
+  sops = {
+    gnupg.home = "/root/.gnupg";
+    defaultSopsFile = ./secrets.yaml;
+    secrets.protonvpn = {
+      sopsFile = ./secrets.yaml;
+    };
   };
 
   networking.wg-quick.interfaces.proton = {
