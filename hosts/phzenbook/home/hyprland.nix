31599e665af36e1fd4f39b0f45987342ecd95c5d
Pierre-Henri <phbasic@gmail.com>
2025-10-10 06:56:44 +0200

    fix: phzenbook screen resolution

--- hosts/phzenbook/home/hyprland.nix
+++ hosts/phzenbook/home/hyprland.nix
@@ -6,5 +6,11 @@ _: {
         kb_variant = "azerty";
       };
     };
+    extraConfig = ''
+      monitor = eDP-1, 3840x2160@30, 0x0, 1.0
+      xwayland {
+        force_zero_scaling = true
+      }
+    '';
   };
 }
