65aee12d11e184642dca2a878892f99940b4907a
Pierre-Henri <phbasic@gmail.com>
2026-06-03 13:31:27 +0200

    fix: bits and bobs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
 
         modules = [
           sops-nix.nixosModules.sops
-          ./modules/vpn.nix
+          # ./modules/vpn.nix
           ./hosts/phantec
 
           home-manager.nixosModules.home-manager
