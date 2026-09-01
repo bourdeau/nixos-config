addfdcd07d2898761c153a48942eb4fc7046d5bf
Pierre-Henri <phbasic@gmail.com>
2026-09-01 15:08:05 +0200

    feat: nvim mapping (#56)

--- home/shell/neovim/nvim/lua/mappings.lua
+++ home/shell/neovim/nvim/lua/mappings.lua
@@ -4,6 +4,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("x", "P", '"0p')
 
 -- Keep visual selection after indenting
 map("v", "<", "<gv")
