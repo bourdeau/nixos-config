4fb5c58b738277095eeac210a4166dd54fe806ce
Pierre-Henri <phbasic@gmail.com>
2026-09-13 15:50:47 +0200

    fix: firewall (#68)

--- modules/system.nix
+++ modules/system.nix
@@ -71,7 +71,11 @@ in {
     };
   };
 
-  networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [22000];
+    allowedUDPPorts = [22000 21027];
+  };
 
   nix = {
     gc = {
