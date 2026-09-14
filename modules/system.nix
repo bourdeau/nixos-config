e2c687ff81d5445f4970fecc286c6eed38b13f73
Pierre-Henri <phbasic@gmail.com>
2026-09-14 10:12:40 +0200

    fix: supported locales

--- modules/system.nix
+++ modules/system.nix
@@ -55,6 +55,10 @@ in {
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "fr_FR.UTF-8/UTF-8"
+    ];
     extraLocaleSettings = {
       LC_ADDRESS = "en_US.UTF-8";
       LC_IDENTIFICATION = "en_US.UTF-8";
